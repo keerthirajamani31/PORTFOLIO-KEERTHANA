e2b19ee11e612659c0cf9e70258c2eca6993117f
keerthirajamani31 <keerthanaanu3103@gmail.com>
2025-11-24 16:36:23 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',  // Change to './' for relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })

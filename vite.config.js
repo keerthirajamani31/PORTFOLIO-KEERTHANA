c976810e3135206538f4479adc363e24ee518ef6
keerthirajamani31 <keerthanaanu3103@gmail.com>
2025-11-24 15:12:31 +0530

    fix: add tailwind config and update deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsInclude: ['**/*.pdf'],
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  publicDir: 'public',
+  base: '/PORTFOLIO-KEERTHANA/', // Your repository name
 })
